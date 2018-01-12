acbf9da62ca4d81d01e10097cd2ee1cb9c3f5119
santigl <santix91@gmail.com>
2018-01-12 19:59:38 -0300

    Add setup.py for distribution. Add extension to LICENSE.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='sqliteanalyzer',
+      version='0.1',
+      description='Extract space-usage statistics from an SQLite database',
+      url='https://github.com/santigl/sqliteanalyzer',
+      license='MIT',
+      packages=['sqliteanalyzer'],
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'Topic :: Database :: Front-Ends',
+                   'Programming Language :: Python :: 3 :: Only',
+                   'License :: OSI Approved :: MIT License'])
